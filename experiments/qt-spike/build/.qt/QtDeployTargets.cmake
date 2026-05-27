510e8f7136a185ae65eb3d6470d1659c08e57db4
EvanLofton <thienthieulkx974@gmail.com>
2026-05-27 14:05:05 +0800

    chore: clean up stray build artifacts and temp files

--- experiments/qt-spike/build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_qt-spike_FILE C:/Program1/RUST/Projects/BetterCPT_V1/experiments/qt-spike/build/qt-spike.exe)
-set(__QT_DEPLOY_TARGET_qt-spike_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_qt-spike_RUNTIME_DLLS C:/Program1/msys64/mingw64/bin/Qt6Widgets.dll;C:/Program1/msys64/mingw64/bin/Qt6Gui.dll;C:/Program1/msys64/mingw64/bin/Qt6Core.dll)
